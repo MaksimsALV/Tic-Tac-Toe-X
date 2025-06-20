f429606391b39be28d296745df280b34286f7bd0
MaksimsALV <maxim.ahmetov@gmail.com>
2025-06-20 20:26:35 +0300

    hello world

---
+++ src/main/java/com/tictactoex/game/TicTacToeXApplication.java
@@ -0,0 +1,13 @@
+package com.tictactoex.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicTacToeXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tictactoex/game/TicTacToeXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tictactoex.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
