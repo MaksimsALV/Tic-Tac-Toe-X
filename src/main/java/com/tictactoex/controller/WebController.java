d4c4960a885dff0c79f1d645fe37b2c3f9f4913a
MaksimsALV <maxim.ahmetov@gmail.com>
2025-06-20 22:30:50 +0300

    controller

--- src/main/java/com/tictactoex/controller/WebController.java
+++ src/main/java/com/tictactoex/controller/WebController.java
@@ -1,4 +1,13 @@
 package com.tictactoex.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WebController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
 }
