2c66299f33968eb0d78be67859f9928b7764e8e5
MaksimsALV <maxim.ahmetov@gmail.com>
2025-06-21 08:47:06 +0300

    board positional variables

--- src/main/java/com/tictactoex/game/Board.java
+++ src/main/java/com/tictactoex/game/Board.java
@@ -7,4 +7,15 @@ public class Board {
             {'7', '8', '9'}
     };
 
+    public static void boardVars() {
+            var pos1 = board[0][0];
+            var pos2 = board[0][1];
+            var pos3 = board[0][2];
+            var pos4 = board[1][0];
+            var pos5 = board[1][1];
+            var pos6 = board[1][2];
+            var pos7 = board[2][0];
+            var pos8 = board[2][1];
+            var pos9 = board[2][2];
+    }
 }
